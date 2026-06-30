14a20349d3e253f3f4ac1c7f5d8ce29937644252
kaka <1771143956@qq.com>
2026-06-30 14:50:37 +0800

    refactor: clean code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,9 +15,10 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 // 读取 .auto-import.json 文件的内容，并将其解析为 JSON 对象
-const autoImportConfig = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, '.auto-import.json'), 'utf-8')
-)
+const autoImportPath = path.resolve(__dirname, '.auto-import.json')
+const autoImportConfig = fs.existsSync(autoImportPath)
+  ? JSON.parse(fs.readFileSync(autoImportPath, 'utf-8'))
+  : {}
 
 export default [
   // 指定文件匹配规则
