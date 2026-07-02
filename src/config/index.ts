34a20a338bcab8f600b94781ffbc4dc64974c86d
kaka <1771143956@qq.com>
2026-07-02 12:53:47 +0800

    fix: 信息修改

--- src/config/index.ts
+++ src/config/index.ts
@@ -38,7 +38,7 @@ import { headerBarConfig } from './modules/headerBar'
 const appConfig: SystemConfig = {
   // 系统信息
   systemInfo: {
-    name: 'xx管理系统' // 系统名称
+    name: '志愿填报管理系统' // 系统名称
   },
   // 系统主题
   systemThemeStyles: {
